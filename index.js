f7c8e5c3a8389ec84f340349ac035f3ae5116300
Josué Lobo <darksiders20005@gmail.com>
2022-06-24 00:09:33 -0300

    feat: implement base code

---
+++ index.js
@@ -0,0 +1,22 @@
+async function trava(textToSpamming) {
+  const lines = textToSpamming.split(/[\n\t]+/).map(line => line.trim()).filter(line => line)
+  const tagMainWhatsApp = document.querySelector("#main")
+  const inputMensage = tagMainWhatsApp.querySelector(`div[contenteditable="true"]`)
+
+  if (!inputMensage) throw new Error("Você tem que estar com uma conversa aberta para usar o script")
+
+  for (const line of lines) {
+    console.log(line)
+
+    inputMensage.textContent = line
+    inputMensage.dispatchEvent(new InputEvent("input", { bubbles: true }));
+
+    (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click()
+
+    if (lines.indexOf(line) !== lines.length - 1) {
+      await new Promise(resolve => setTimeout(resolve, 250))
+    }
+  }
+
+  return lines.length
+}
